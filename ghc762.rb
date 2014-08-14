b02a615a23d4773e6c913648f37168427594c590
Rodlogic <admin@rodlogic.net>
2014-08-14 18:20:14 -0300

    fixed wrong version number

--- ghc762.rb
+++ ghc762.rb
@@ -4,7 +4,7 @@ require 'formula'
     homepage "http://haskell.org/ghc/"
     url "http://www.haskell.org/ghc/dist/7.6.2/ghc-7.6.2-x86_64-apple-darwin.tar.bz2"
     sha1 "c5f2c36badf2a1c79259ccb1b1f0dcb8ff801356"
-    version '7.6.2-x86_64'
+    version '7.6.2'
 
     # TODO 32-bits
     resource "x86" do
