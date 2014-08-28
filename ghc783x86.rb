9a5344a73c626ca84bf18902f7fd170ea78f6c3a
Rodlogic <admin@rodlogic.net>
2014-08-27 21:03:49 -0300

    7.8.3x86 build from source still not working

--- ghc783x86.rb
+++ ghc783x86.rb
@@ -6,12 +6,15 @@ require 'formula'
     sha1 "4d7598f8bff3211708da654ec6dd342a2c95dc58"
     version '7.8.3x86'
 
+    option "32-bit"
+
     # http://hackage.haskell.org/trac/ghc/ticket/6009
     depends_on :macos => :snow_leopard
     depends_on "gmp"
+    depends_on "ghc783" => :build
 
     # These don't work inside of a `stable do` block
-    if MacOS.version < :mavericks
+    if MacOS.version < :mavericks || build.build_32_bit?
       depends_on "gcc" if MacOS.version >= :mountain_lion
       env :std
 
@@ -25,7 +28,7 @@ require 'formula'
 
     def install
       # ensure configure does not use Xcode 5 "gcc" which is actually clang
-      system "./configure --prefix=#{prefix} --with-gcc=#{ENV.cc} --target=i386-apple-darwin"
+      system "./configure --prefix=#{prefix} --with-gcc=#{ENV.cc} --target=i386-apple-darwin --with-ld=/usr/bin/ld --with-nm=/usr/bin/nm --with-ar=/usr/bin/ar --with-ranlib=/usr/bin/ranlib"
       if MacOS.version <= :lion
         # __thread is not supported on Lion but configure enables it anyway.
         File.open("mk/config.h", "a") do |file|
